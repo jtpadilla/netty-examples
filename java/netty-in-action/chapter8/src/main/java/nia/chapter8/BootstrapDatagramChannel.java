02b77224cf5e651115c96b7499c015f5e8ba529b
Juan Padilla <padilla@speedycontrol.com>
2021-10-22 14:54:38 +0200

    Ejemplos

--- java/netty-in-action/chapter8/src/main/java/nia/chapter8/BootstrapDatagramChannel.java
+++ java/netty-in-action/chapter8/src/main/java/nia/chapter8/BootstrapDatagramChannel.java
@@ -5,40 +5,34 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.DatagramPacket;
-import io.netty.channel.socket.oio.OioDatagramChannel;
+import io.netty.channel.socket.nio.NioDatagramChannel;
 
 import java.net.InetSocketAddress;
 
-/**
- * Listing 8.8 Using Bootstrap with DatagramChannel
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
- * @author <a href="mailto:mawolfthal@gmail.com">Marvin Wolfthal</a>
- */
 public class BootstrapDatagramChannel {
 
-    /**
-     * Listing 8.8 Using Bootstrap with DatagramChannel
-     */
+    public static class MySimpleChannelInboundHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+
+        @Override
+        public void channelRead0(ChannelHandlerContext ctx,DatagramPacket msg) throws Exception {
+        }
+
+    }
+
     public void bootstrap() {
+
         Bootstrap bootstrap = new Bootstrap();
-        bootstrap.group(new OioEventLoopGroup()).channel(
-            OioDatagramChannel.class).handler(
-            new SimpleChannelInboundHandler<DatagramPacket>() {
-                @Override
-                public void channelRead0(ChannelHandlerContext ctx,
-                    DatagramPacket msg) throws Exception {
-                    // Do something with the packet
-                }
-            }
-        );
+
+        bootstrap
+            .group(new NioEventLoopGroup())
+            .channel(NioDatagramChannel.class)
+            .handler(new MySimpleChannelInboundHandler());
+
         ChannelFuture future = bootstrap.bind(new InetSocketAddress(0));
-        future.addListener(new ChannelFutureListener() {
-            @Override
-            public void operationComplete(ChannelFuture channelFuture)
-               throws Exception {
+
+        future.addListener((ChannelFutureListener) channelFuture -> {
                if (channelFuture.isSuccess()) {
                    System.out.println("Channel bound");
                } else {
@@ -46,6 +40,7 @@ public class BootstrapDatagramChannel {
                    channelFuture.cause().printStackTrace();
                }
             }
-        });
+        );
+
     }
 }
