38aa9bee95994592b8b96fc4b596ca47168981f1
Juan Padilla <padilla@speedycontrol.com>
2021-10-28 14:35:35 +0200

    Ejemplos

--- java/netty-in-action/chapter9/src/test/java/nia/test/chapter9/AbsIntegerEncoderTest.java
+++ java/netty-in-action/chapter9/src/test/java/nia/test/chapter9/AbsIntegerEncoderTest.java
@@ -13,21 +13,26 @@ public class AbsIntegerEncoderTest {
     @Test
     public void testEncoded() {
 
+        // Se prepara el buffer con los datos a transmitir
         ByteBuf buf = Unpooled.buffer();
-
         for (int i = 1; i < 10; i++) {
             buf.writeInt(i * -1);
         }
 
+        // Se cera el canal con el encoder
         EmbeddedChannel channel = new EmbeddedChannel(new AbsIntegerEncoder());
+
+        // Se transmiten los datos que tenemos en el buffer
         assertTrue(channel.writeOutbound(buf));
         assertTrue(channel.finish());
 
+        // Se leen lo valores y se verifican que son los esperados.
         for (int i = 1; i < 10; i++) {
             Integer intValue  = channel.readOutbound();
             assertEquals(i, intValue.intValue());
         }
 
+        // Ahora la lectura tiene que retornar un null
         assertNull(channel.readOutbound());
 
     }
--- java/netty-in-action/chapter9/src/test/java/nia/test/chapter9/FrameChunkDecoderTest.java
+++ java/netty-in-action/chapter9/src/test/java/nia/test/chapter9/FrameChunkDecoderTest.java
@@ -10,42 +10,51 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Listing 9.6 Testing FrameChunkDecoder
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
- */
-
 public class FrameChunkDecoderTest {
+
     @Test
     public void testFramesDecoded() {
+
+        // Se prepara un buffer con 9 bytes
         ByteBuf buf = Unpooled.buffer();
         for (int i = 0; i < 9; i++) {
             buf.writeByte(i);
         }
+
+        // Se genera un duplicado del buffer (compartiendo el buffer subyacente)
         ByteBuf input = buf.duplicate();
 
-        EmbeddedChannel channel = new EmbeddedChannel(
-            new FrameChunkDecoder(3));
+        // Se crea un canal con un encoder que limita los bloques a un maximo de 3 bytes
+        EmbeddedChannel channel = new EmbeddedChannel(new FrameChunkDecoder(3));
 
+        // Se transmite un bloque de 2 bytes y toddo tiene que ir bien.
         assertTrue(channel.writeInbound(input.readBytes(2)));
+
+        // Se transmite un bloque de 4 bytes y se espera una excepcion
         try {
             channel.writeInbound(input.readBytes(4));
             Assert.fail();
         } catch (TooLongFrameException e) {
             // expected exception
         }
+
+        // Se transmite un bloque de 3 bytes y toddo tiene que ir bien.
         assertTrue(channel.writeInbound(input.readBytes(3)));
         assertTrue(channel.finish());
 
-        // Read frames
+        // Se lee el primer bloque de 2 bytes y se compara con el original
         ByteBuf read = (ByteBuf) channel.readInbound();
         assertEquals(buf.readSlice(2), read);
         read.release();
 
+        // Hay retira del original los 4 bytes que no entraton
+        // y se leen los 3 bytes que se pusieron despues.
         read = (ByteBuf) channel.readInbound();
         assertEquals(buf.skipBytes(4).readSlice(3), read);
         read.release();
+
+        // Se libera el buffer
         buf.release();
+
     }
 }
